2bdec596886e84596d2dd460947287908669e2aa
Rene Kiessling <rkiessli@tibco.com>
2022-10-31 18:17:34 +0100

    corrections for container to be stopped

--- stopDatadogAgent.sh
+++ stopDatadogAgent.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 # terminating and deleting dd-agent container on local docker
 
-if [ $(docker container ls --filter "status=running" --filter "ancestor=gcr.io/datadoghq/agent:7" | wc -l) > 1  ]
+if [[ $(docker container ls --filter "status=running" --filter "ancestor=gcr.io/datadoghq/agent:7" | wc -l) > 1  ]]
 then
   echo "[INFO] stopping dd-agent container"
   docker stop dd-agent
 fi
 
-if [ $(docker container ls --filter "status=exited" --filter "ancestor=gcr.io/datadoghq/agent:7" | wc -l) > 1  ]
+if [[ $(docker container ls --filter "status=exited" --filter "ancestor=gcr.io/datadoghq/agent:7" | wc -l) > 1  ]]
 then
   echo "[INFO] deleting dd-agent container"
   docker rm dd-agent
