64b1002fccd2db9ddcd62480ee03a40a89848e30
Rene Kiessling <rkiessli@tibco.com>
2022-11-17 15:00:27 +0100

    additions for OTLP vai Datadog

--- startDatadogAgent.sh
+++ startDatadogAgent.sh
@@ -16,6 +16,12 @@ fi
 # - app metrics realy:    -p 8126:8126/tcp
 # dd-agent is starting to report to DD cloud servers on metrics for the local server,
 # docker infrastructure and any app connected to it.
+#
+# Datadog Documentation on OpenTelemtry (OLTP): 
+#  [Send Metrics from OpenTelemetry to Datadog](https://docs.datadoghq.com/metrics/open_telemetry/#overview)
+#  [Enabling OTLP Ingestion on the Datadog Agent](https://docs.datadoghq.com/tracing/trace_collection/open_standards/otlp_ingest_in_the_agent/?tab=docker#enabling-otlp-ingestion-on-the-datadog-agent)
+#  For gPRC: Set DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT to 0.0.0.0:4317 and expose port 4317.
+#  For HTTP: Set DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT to 0.0.0.0:4318 and expose port 4318.
 echo "[INFO] Starting a new dd-agent container"
 docker run -d \
  --name dd-agent \
@@ -26,10 +32,14 @@ docker run -d \
  -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
  -p 8125:8125/udp \
  -p 8126:8126/tcp \
+ -p 4317:4317/tcp \
+ -p 4318:4318/tcp \
  -e DD_API_KEY=${DATADOG_API_KEY} \
  -e DD_SITE=${DATADOG_SITE} \
  -e DD_LOGS_ENABLED=true \
  -e DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL=true \
+ -e DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT=0.0.0.0:4317 \
+ -e DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_HTTP_ENDPOINT=0.0.0.0:4318 \
  gcr.io/datadoghq/agent:7
 
 echo "[INFO] waiting for dd-agent container initialization"
